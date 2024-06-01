03730eee607123fc00661737d57a139a74342669
valeogre <alex.ogrezeanu123@gmail.com>
2024-06-01 17:24:49 +0300

    sters niste comentarii aiurea

--- partea1_2.py
+++ partea1_2.py
@@ -7,36 +7,28 @@ train_data_in = pd.read_csv('train.csv')
 
 # determine the number of columns of the file
 columns_num = train_data_in.shape[1]
-# print(f'Number of columns is: {columns_num}')
+
 
 # determine the data types of each column
 data_types = []
 for column in train_data_in.columns:
     data_types.append((column, train_data_in[column].dtype))
-# print('\nThe types of data of each column:')
-# for column, dtype in data_types:
-#    print(f'{column}: {dtype}')
 
 # number of missing values of each column
 missing_val = []
 for col in train_data_in.columns:
     missing_val.append((col, train_data_in[col].isnull().sum()))
 
-# print('\nNumber of missing values for each column:')
-# for column, miss in missing_val:
-#    print(f'{column}:{miss}')
-
 # number of lines
 rows_num = train_data_in.shape[0]
-# print(f'\nNumber of rows is: {rows_num}')
 
 # number of duplicate lines
 duplicated_lines = train_data_in.duplicated().sum()
-# print(f'\nNumber of duplicate lines: {duplicated_lines}')
 
 # number of passangers that survived and of the ones that died
 survived = train_data_in['Survived'].sum()
 didnt_survive = rows_num - survived
+
 # percentage of the people that survived or died
 percentage_surv = 100 * survived / rows_num
 percentage_died = 100 * didnt_survive / rows_num
